c8c0acefbd38aed8b0c1dfb06c46088e9b0c3a24
puppyriver <4278246@qq.com>
2016-05-05 21:15:16 +0800

    v

--- webapp/src/js/entry/test.jsx
+++ webapp/src/js/entry/test.jsx
@@ -1,7 +1,15 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import BTable from '../component/BTable'
 //import Button from 'react-bootstrap/lib/Button';
 //// or
 import { Button } from 'react-bootstrap';
 
-ReactDOM.render(<Button bsStyle="primary" bsSize="large">Large button</Button>,document.getElementById('react-content'));
+ReactDOM.render(<div>
+
+        <Button bsStyle="primary" bsSize="large">Large button</Button>
+    <BTable></BTable>
+    </div>
+
+
+    ,document.getElementById('react-content'));
