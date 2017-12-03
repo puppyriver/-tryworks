2cf02fd676c164df2909b5f05293dfbac1582125
puppyriver <4278246@qq.com>
2017-12-03 12:30:59 +0800

    11

--- python-test/split_dir.py
+++ python-test/split_dir.py
@@ -3,21 +3,23 @@ import os.path
 import shutil
 
 # this folder is custom
-rootdir = "D:/works"
-
+rootdir = "H:\mumu_pictures\camera20171007"
+SPLIT_LENGTH = 3000
 # for f in os.walk(rootdir):
 #     print(f)
-idx = 0;
+
 for parent, dirnames, filenames in os.walk(rootdir):
-    fullName = os.path.join(parent, filename)
-    if idx == 100 :
-        shutil.move(os.path.join(parent, filename),os.path.join(parent, filename))
-        idx = 0
-    # case 1:
     for dirname in dirnames:
         print("parent folder is:" + parent)
         print("dirname is:" + dirname)
         # case 2
+    idx = 0;
     for filename in filenames:
-        print("parent folder is:" + parent)
-        print("filename with full path:" + os.path.join(parent, filename))
+        # print("parent folder is:" + parent)
+        # print("filename with full path:" + os.path.join(parent, filename))
+        idx = idx + 1
+        if True:
+            destDir = parent + "." + str(int(idx / SPLIT_LENGTH))
+            if not(os.path.exists(destDir)):
+                os.mkdir(destDir)
+            shutil.move(os.path.join(parent, filename), os.path.join(destDir, filename))
